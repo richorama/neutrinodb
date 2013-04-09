93061ebe6131a9118ea75e4fd1aae8e2c74fc713
Richard Astbury <richard.astbury@gmail.com>
2013-04-09 03:55:37 -0700

    get entity

--- server.js
+++ server.js
@@ -14,7 +14,22 @@ app.get("/", function(req, res){
 });
 
 app.get("/:table", function(req, res){
+	var options = parseOptions(req);
+	db.readRange(req.params.table, options, function(error, data){
+		if (error) console.log(error);
+		res.send(data);
+	});
+});
 
+app.get("/:table/:key", function(req, res){
+	db.get(req.params.table, req.params.key, function(error, data){
+		res.send(data);
+	});
+});
+
+
+
+function parseOptions(req){
 	var options = {skip:0};
 	if (req.query["$skip"]){
 		options.skip = parseInt(req.query["$skip"]);
@@ -31,12 +46,8 @@ app.get("/:table", function(req, res){
 		var ast = odParser.parse("$filter=" + req.query["$filter"]);
 		options.filter = ast["$filter"];
 	}
-
-	db.readRange(req.params.table, options, function(error, data){
-		if (error) console.log(error);
-		res.send(data);
-	});
-});
+	return options;
+}
 
 /*
 	var tableName = req.params[0];
